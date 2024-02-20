a564470ff0bd83a6b655dbee3ed6bcabf4feceef
Illia Sakharau <sakharovia0204@gmail.com>
2024-02-20 12:58:32 +0400

    feat: implement - user validation

--- backend/actions/registration.ts
+++ backend/actions/registration.ts
@@ -7,12 +7,41 @@ type Props = {
   ws: WebSocket;
 }
 
+let idCounter = 0;
+
 export const registration = ({ action, users, ws }: Props) => {
   const data = JSON.parse(action.data);
+
+  if (data.name.length < 5 || data.password.length < 5) {
+    ws.send(JSON.stringify({
+      type: "reg",
+      data: JSON.stringify(
+        {
+          error: true,
+          errorText: 'Invalid name or password',
+        }),
+      id: action.id,
+    }))
+    return;
+  }
+  const hasUserName = !Array.from(users.values()).every((user) => !(user.name === data.name))
+  if (hasUserName) {
+    ws.send(JSON.stringify({
+      type: "reg",
+      data: JSON.stringify(
+        {
+          error: true,
+          errorText: 'A user with this name exists',
+        }),
+      id: action.id,
+    }))
+    return;
+  }
+
   const user = users.set(ws, {
-    id: users.size,
+    id: idCounter++,
     ...data
-  }).get(ws)
+  }).get(ws)  
   
   ws.send(JSON.stringify({
     type: "reg",
